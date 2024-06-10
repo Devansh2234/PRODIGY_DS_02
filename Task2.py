6b675bd7c9d573e740c198af29dc746b95fe5735
Devansh2234 <126373450+Devansh2234@users.noreply.github.com>
2024-06-10 11:28:53 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,13 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+import keras
+import sklearn
+import os
+import re
+
+
+df = pd.read_csv("")
+df.head()
