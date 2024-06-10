d8a0065ddb5d480bb0cb2282bc9a886aac4201b2
Devansh2234 <126373450+Devansh2234@users.noreply.github.com>
2024-06-10 11:31:18 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -9,5 +9,18 @@ import os
 import re
 
 
-df = pd.read_csv("")
+df = pd.read_csv("energy_dataset_.csv")
 df.head()
+
+missing_values = df.isnull().sum()
+print(missing_values)
+sns.set(style="whitegrid")
+
+# Installed Capacity of Different Types of Renewable Energy
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Type_of_Renewable_Energy', y='Installed_Capacity_MW', data=df, ci=None, palette='viridis')
+plt.title('Installed Capacity of Different Types of Renewable Energy')
+plt.xlabel('Type of Renewable Energy')
+plt.ylabel('Installed Capacity (MW)')
+plt.xticks(ticks=[0, 1, 2, 3, 4, 5, 6], labels=['Solar', 'Wind', 'Hydroelectric', 'Geothermal', 'Biomass', 'Tidal', 'Wave'])
+plt.show()
